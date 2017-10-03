94646547ab4c52c223be7279fccc7d47479a8920
Sam <swkane@indiana.edu>
2017-10-03 01:10:47 -0400

    rough mockup of homepage, no color

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
+import 'semantic-ui-css/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
+import BaseLayout from './components/BaseLayout';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BaseLayout>
+    <App />
+  </BaseLayout>
+
+  , document.getElementById('root'));
 registerServiceWorker();
