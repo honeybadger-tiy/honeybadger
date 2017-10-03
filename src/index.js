e433cf2c8cc0ce2d1f88ef9893d6e0ac9c765acf
Sam <swkane@indiana.edu>
2017-10-03 11:50:24 -0400

    scaffolding structure of badges page

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import HomePage from './components/HomePage';
 import 'semantic-ui-css/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
 import BaseLayout from './components/BaseLayout';
+import BadgesPage from './components/BadgesPage';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <BaseLayout>
-    <HomePage />
-  </BaseLayout>
-
+  <BrowserRouter>
+    <BaseLayout>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/badges' component={BadgesPage} />
+      </Switch>
+    </BaseLayout>
+  </BrowserRouter>
   , document.getElementById('root'));
 registerServiceWorker();
