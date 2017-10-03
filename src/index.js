50efaaf0527dd1106879855e50cf2e804d9802b6
Sam <swkane@indiana.edu>
2017-10-03 09:44:09 -0400

    changed name of app.js to homepage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './components/App';
+import App from './components/HomePage';
 import 'semantic-ui-css/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
 import BaseLayout from './components/BaseLayout';
